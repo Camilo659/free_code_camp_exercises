3883f108937158ddf5e79db56fe0075d3843f667
Camilo659 <85297828+Camilo659@users.noreply.github.com>
2022-10-16 03:22:18 -0500

    Update arithmetic_arranger_function.py
    I did not figure out how to put the operations nextdoor each other instead vertical sense.

--- arithmetic_arranger_function.py
+++ arithmetic_arranger_function.py
@@ -1,48 +1,130 @@
-ecuation = ['123 + 1', '123 - 1', '123 * 1', '123 / 1']
+from sys import breakpointhook
 
-#Know the arithmetic sign and rewrite
-def arithmetic_arranger(ecuation, solve):
-    for i in ecuation:
+
+def arithmetic_arranger(equation, with_result):
+    counter = 1
+    sum = 0
+    rest = 0
+    
+
+    for i in equation:
+        if counter > 5:
+            print('Error: Too many problems')
+            breakpointhook
+
 
 
         try:
-            if i.index('+') > 0:
-                if solve == True:
-                    result = i.replace('+', ' ')  
-                    result = result.split()              
-                    result = [int(x) for x in result]    #Strings becomes an Ints
-                    result = sum(result)
-                    print(i.replace(' ', '\n', 1) ,'\n---', '\n', result)
+            if i.index('+') > 0:                                                                
+                
+                #Separating the Strign and becoming it in a list
+                if with_result == True:
+                    operation = i.replace('+', ' ') 
+                    operation = operation.split()
+                
+                #Checking the String does not have digits
+                    for x in operation:
+                        if len(x) > 4:
+                            print("Error: Numbers cannot be more than four digits.")
+                            breakpointhook
 
-                else:print(i.replace(' ', '\n',1),'\n------')
+                    first = operation[0]
+                    second = operation[1]
+                    
+                    #From String to Int
+                    try:
+                        operation = [int(x) for x in operation]                     
+                    except:print("Error: Numbers must only contain digits.")
+
+                    for x in operation:
+                        sum = x + sum
+                    
+
+                    print("{:>6}".format(first),'\n','+{:>4}'.format(second),"\n", "{:>5}".format('------'),"\n","{:>5}".format(sum), '\n\n\n')
+                    
+                #The same above but if there is a False parameter
+                elif with_result == False:
+                    operation = i.replace('+', ' ') 
+                    operation = operation.split()
+
+                    for x in operation:
+                        if len(x) > 4:
+                            print("Error: Numbers cannot be more than four digits.")
+                            breakpointhook
+
+                    first = operation[0]
+                    second = operation[1]
+                    try:
+                        operation = [int(x) for x in operation]    
+                    except:print("Error: Numbers must only contain digits.")
+                    print("{:>6}".format(first),'\n','+{:>4}'.format(second),"\n", "{:>5}".format('------'), '\n\n\n')
+
+
+                else:pass
         except:pass
+
+
 
         try:
             if i.index('-') > 0:
-                if solve == True:
-                    rest = 0
-                    result = i.replace('-', ' ')  
-                    result = result.split()              
-                    result = [int(x) for x in result]
-                    for x in result:
+
+                #Same above but with negative operations
+                if with_result == True:
+                    operation = i.replace('-', ' ')  
+                    operation = operation.split()
+
+                    for x in operation:
+                        if len(x) > 4:
+                            print("Error: Numbers cannot be more than four digits.")
+                            breakpointhook    
+
+                    try:
+                        operation = [int(x) for x in operation]                           
+                    except:print("Error: Numbers must only contain digits.")
+
+                    for x in operation:
                         rest = x - rest
                     rest *= -1
-                    print(i.replace(' ', '\n', 1) ,'\n---', '\n', rest)
 
-                else:print(i.replace(' ', '\n',1),'\n------')  
+                    first = operation[0]
+                    second = operation[1]
+
+                    print("{:>6}".format(first),'\n','-{:>4}'.format(second),"\n", "{:>5}".format('------'),"\n","{:>5}".format(rest), '\n\n\n')
+
+
+                elif with_result == False:
+                    operation = i.replace('-', ' ') 
+                    operation = operation.split()
+
+                    for x in operation:
+                        if len(x) > 4:
+                            print("Error: Numbers cannot be more than four digits.")
+                            breakpointhook
+
+                    first = operation[0]
+                    second = operation[1]
+
+                    try:
+                        operation = [int(x) for x in operation]    
+                    except:print("Error: Numbers must only contain digits.")
+
+                    print("{:>6}".format(first),'\n','-{:>4}'.format(second),"\n", "{:>5}".format('------'), '\n\n\n')
+
+
+                else:pass 
         except:pass
+
 
 
         try:
             if i.index('*') > 0:
-                print('Error: Too many problems')
+                print("Error: Operator must be '+' or '-'")
         except:pass
         try:
             if i.index('/') > 0:
-                print('Error: Too many problems')
+                print("Error: Operator must be '+' or '-'")
         except:pass
-
-
-
-arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True)
+        counter += 1
+        sum = 0
+        rest = 0
 
